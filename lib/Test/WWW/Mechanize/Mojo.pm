3972d58e76d3be30eae3c3955c2d8a9b589bdcc3
Shlomi Fish <shlomif@shlomifish.org>
2014-02-02 09:15:59 +0000

    Import of SHLOMIF/Test-WWW-Mechanize-Mojo-v0.0.16 from CPAN.
    gitpan-cpan-distribution: Test-WWW-Mechanize-Mojo
    gitpan-cpan-version:      v0.0.16
    gitpan-cpan-path:         SHLOMIF/Test-WWW-Mechanize-Mojo-v0.0.16.tar.gz
    gitpan-cpan-author:       SHLOMIF
    gitpan-cpan-maturity:     released

--- lib/Test/WWW/Mechanize/Mojo.pm
+++ lib/Test/WWW/Mechanize/Mojo.pm
@@ -13,7 +13,7 @@ use base 'Test::WWW::Mechanize';
 
 use Test::Mojo;
 
-our $VERSION = '0.0.15';
+our $VERSION = '0.0.16';
 
 our $APP_CLASS;
 my $Test = Test::Builder->new();
@@ -196,7 +196,7 @@ sub _do_mojo_request {
 
     # Client
     my $client = $t->ua;
-    $client->app($t->app);
+    $client->server->app($t->app);
 
     my $method = lc($request->method());
 
